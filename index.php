3511787e054ec4a80f34f84e26d82d853b446e6b
Nemanja Dimitrijevic <nemanja94dev@gmail.com>
2019-10-03 13:15:56 +0200

    File for logging out logged in user

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+include_once "config/core.php";
+
+$page_title = "Index";
+
+$require_login = true;
+include_once "login_checker.php";
+
+include_once 'layout_head.php';
+
+echo "<div class='col-md-12'>";
+
+  $action = isset($_GET['action']) ? $_GET['action'] : "";
+
+  if($action == "login_success") {
+    echo "<div class='alert alert-info'>";
+      echo "<strong>Hi " . $_SESSION['firstname'] . ", welcome back!</strong>";
+    echo "</div>";
+  }
+
+  else if($action == 'already_logged_in') {
+    echo "<div class='alert alert-info'>";
+       echo "<strong>You are already logged in</strong>";
+    echo "</div>";
+  }
+
+  echo "<div class='alert alert-info'>";
+   echo "Content when logged in will be here. For example, your premium of products or services.";
+  echo "</div>";
+
+  echo "</div>";
+
+  include 'layout_foot.php';
+
+ ?>
