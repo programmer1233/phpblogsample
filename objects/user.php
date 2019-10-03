3fa98b54b116159f42aefc7f7e0214563e7088e5
Nemanja Dimitrijevic <nemanja94dev@gmail.com>
2019-10-03 10:52:18 +0200

    File for apps home URL and pagination settings

---
+++ objects/user.php
@@ -0,0 +1,32 @@
+<?php
+class User {
+
+  // database connection and table name
+  private $conn;
+  private $table_name = "users";
+
+  // object properties
+  public $id;
+  public $firstname;
+  public $lastname;
+  public $email;
+  public $contact_number;
+  public $address;
+  public $password;
+  public $access_level;
+  public $access_code;
+  public $status;
+  public $created;
+  public $modified;
+
+  public function __construct($db) {
+    $this->conn = $db;
+  }
+}
+
+
+
+
+
+
+ ?>
