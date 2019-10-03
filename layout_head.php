95800a3e0c2195c8b09703477edafde6659cbcf6
Nemanja Dimitrijevic <nemanja94dev@gmail.com>
2019-10-03 11:39:43 +0200

    File that contains HTML header code

---
+++ layout_head.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PDO - Create a Record - PHP CRUD Tutorial</title>
+    <!-- bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <!-- custom CSS -->
+    <link rel="stylesheet" href="libs/css/custom.css">
+  </head>
+  <body>
+
+    <!-- container -->
+    <div class="container">
+      <?php
+      // show page header
+      echo "<div class='page-header'>
+         <h1>{$page_title}</h1>
+            </div>";
+       ?>
