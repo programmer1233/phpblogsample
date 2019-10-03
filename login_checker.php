4444c43f33c64bb944b55e4d84e952cbd36d120b
Nemanja Dimitrijevic <nemanja94dev@gmail.com>
2019-10-03 11:06:30 +0200

    The .htaccess file is very useful to make URLs pretty and easy to remember

---
+++ login_checker.php
@@ -0,0 +1,23 @@
+<?php
+
+
+if(isset($_SESSION['access_level']) && $_SESSION['access_level']=="Admin") {
+  header("Location: {$home_url}admin/index.php?action=logged_in_as_admin");
+}
+
+else if(isset($require_login) && $require_login == true) {
+  if(!isset($_SESSION['access_level'])) {
+    header("Location: {$home_url}login.php?action=please_login");
+  }
+}
+
+else if(isset($page_title) && ($page_title=="Login" || $page_title=="Sign Up")) {
+  if(isset($_SESSION['access_level']) && $_SESSION['access_level'] == "Customer") {
+    header("Location: {$home_url}index.php?action=already_logged_in");
+  }
+}
+
+else {
+  
+}
+ ?>
