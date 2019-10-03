2524475187093f555967ba987ad3278c033a9d27
Nemanja Dimitrijevic <nemanja94dev@gmail.com>
2019-10-03 11:41:18 +0200

    File that contains HTML footer code

---
+++ layout_foot.php
@@ -0,0 +1,11 @@
+</div>
+<!-- container -->
+
+
+<!-- jquery -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<!-- bootstrap JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+
+  </body>
+</html>
--- layout_head.php
+++ layout_head.php
@@ -2,19 +2,24 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>PDO - Create a Record - PHP CRUD Tutorial</title>
+    <title><?php echo isset($page_title) ? strip_tags($page_title) : "Store Front"; ?></title>
     <!-- bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
     <!-- custom CSS -->
-    <link rel="stylesheet" href="libs/css/custom.css">
+    <link rel="stylesheet" href="<?php echo $home_url . "/libs/css/customer.css" ?>">
   </head>
   <body>
 
     <!-- container -->
     <div class="container">
       <?php
-      // show page header
-      echo "<div class='page-header'>
-         <h1>{$page_title}</h1>
-            </div>";
+      if($page_title != "Login"){
        ?>
+       <div class="col-md-12">
+        <div class="page-header">
+          <h1><?php echo isset($page_title) ? $page_title : "PHP Blog"; ?></h1>
+        </div>
+       </div>
+       <?php
+        }
+        ?>
