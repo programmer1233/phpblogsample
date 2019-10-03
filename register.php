2c5a188187d2325fd27d5b230c6fdfd30e7239ab
Nemanja Dimitrijevic <nemanja94dev@gmail.com>
2019-10-03 13:20:10 +0200

    Register page is where people will enter their information for registration

---
+++ register.php
@@ -0,0 +1,21 @@
+<?php
+include_once "config/core.php";
+
+$page_title = "Register";
+
+include_once "login_checker.php";
+
+include_once 'config/database.php';
+include_once 'objects/user.php';
+include_once 'libs/php/utils.php';
+
+include_once "layout_head.php";
+
+echo "<div class='col-md-12'>";
+
+echo "</div>";
+
+include_once "layout_foot.php";
+
+
+ ?>
