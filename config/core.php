b2632952f7411b18392c96e11dd9f5e4d5cf20f6
Nemanja Dimitrijevic <nemanja94dev@gmail.com>
2019-10-03 11:03:49 +0200

    Check if the user is logged in. If is not, he gets information that he needs to login if he wants to read contents of blog

---
+++ config/core.php
@@ -0,0 +1,18 @@
+<?php
+error_reporting(E_ALL);
+
+session_start();
+
+date_default_timezone_set('Europe/Belgrade');
+
+$home_url = "http://localhost/php-blog/";
+
+$page = isset($_GET['page']) ? $_GET['page'] : 1;
+
+$records_per_page = 5;
+
+$from_record_num = ($records_per_page * $page) - $records_per_page;
+
+
+
+ ?>
